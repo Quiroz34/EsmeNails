e57e4ff4db3feda3d431edf860bbf67cf18f123c
Carlos Quiroz <quirozzap34@gmail.com>
2025-10-01 17:40:47 -0600

    actualizacion carrusel, cita, diseño y js

--- js/carruselI.js
+++ js/carruselI.js
@@ -2,12 +2,21 @@ const carousel = document.getElementById("carousel");
 const slides = carousel.children.length;
 let index = 0;
 
+const dots = document.querySelectorAll(".dot");
+
 function showSlide(i) {
   index = (i + slides) % slides;
   carousel.style.transform = `translateX(-${index * 100}%)`;
+
+  dots.forEach(dot => dot.classList.remove("active"));
+  dots[index].classList.add("active");
 }
 
 document.getElementById("prev").addEventListener("click", () => showSlide(index - 1));
 document.getElementById("next").addEventListener("click", () => showSlide(index + 1));
 
-setInterval(() => showSlide(index + 1), 4000); // cambio automático cada 4s
+dots.forEach((dot, i) => dot.addEventListener("click", () => showSlide(i)));
+
+setInterval(() => showSlide(index + 1), 4000);
+
+showSlide(index);
